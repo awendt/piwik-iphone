8a8df31a2e3a1f92d75690e8a844bc667c801719
André Wendt <awendt@freeshell.org>
2011-03-14 21:36:24 +0100

    Refactoring: Module

---
+++ piwik_iphone.js
@@ -0,0 +1,26 @@
+var piwik_iphone = (function() {
+  var that = {
+    url_for: function(options) {
+      var settings = {
+        install: localStorage.install,
+        format: 'json',
+        token_auth: localStorage.token,
+        idSite: localStorage.site,
+      };
+      $.extend(settings, options);
+      var install = settings.install;
+      delete settings.install;
+
+      var query_elements = [];
+      $.each(settings, function(key, value) {
+        if (value) {
+          query_elements.push(key+"="+value);
+        }
+      });
+      query_elements.push("jsoncallback=?");
+      return install+"/?module=API&" + query_elements.join("&");
+    }
+  };
+
+  return that;
+}());
